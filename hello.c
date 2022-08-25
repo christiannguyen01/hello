a0613c6a785251dfcd284f4eff82e4ee7645904e
christiannguyen01 <cnguyen31@huskers.unl.edu>
2022-08-25 15:00:22 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+Author: Christian Nguyen
+        Cnguyen31@huskers.unl.edu
+Date: 2022/08/25
+A hello program for git
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf ("Hello World, I'm Christian Nguyen!\n");
+    printf ("Computer Science is my major!\n");
+
+    return 0;
+}
