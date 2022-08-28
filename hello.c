562ad5e1d0f321c8e8c8ff4af4f0c88a7380bfd5
christiannguyen1 <christiannguyen1001@gmail.com>
2022-08-28 23:23:21 +0000

    first commit

--- hello.c
+++ hello.c
@@ -13,6 +13,9 @@ int main(int argc, char **argv)
 {
   printf("Hello World, I'm Christian Nguyen!\n");
   printf("My major is Computer Science \n");
+  printf("Hello World, I'm also Christian Nguyen!"\n);
+  printf("Same person same major! I didn't have a partner at the time of doing it b/c I thought it was optional with the if statements on directions\n");
+  printf("I saw an email saying I needed one but it's a bit late for that so I'm putting myself in.\n");
 
   return 0;
 }
